7dad05e803ab8e3d4c1579f5745fa75114e2ff93
nicholas-stancio-saka <work.nicholasstancio@gmail.com>
2023-11-28 20:47:24 +0700

    Adjust ProductCard

--- lib/features/product_list/presentation/widgets/product_card.dart
+++ lib/features/product_list/presentation/widgets/product_card.dart
@@ -1,17 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 import 'package:shop_it/features/product_list/domain/entities/product.dart';
 
 class ProductCard extends StatelessWidget {
   final Product product;
   final void Function()? onTap;
+  final bool isAlternate;
 
-  const ProductCard({required this.product, this.onTap, super.key});
+  const ProductCard({
+    required this.product,
+    this.onTap,
+    this.isAlternate = false,
+    super.key,
+  });
 
   @override
   Widget build(BuildContext context) {
-    // Get the width of the card to set the image height
-    double width = MediaQuery.of(context).size.width / 2;
-
     return GestureDetector(
       onTap: onTap,
       child: Card(
@@ -24,68 +28,115 @@ class ProductCard extends StatelessWidget {
           borderRadius: BorderRadius.circular(10.0),
           child: Container(
             color: Colors.white,
+            child: !isAlternate ? _buildMainCardContent(context) : _buildAlternateCardContent(),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget _buildMainCardContent(BuildContext context) {
+    return LayoutBuilder(
+      builder: (context, constraints) {
+        final cardWidth = constraints.maxWidth;
+
+        return Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            SizedBox(
+              width: cardWidth,
+              height: cardWidth * 0.9, // 90 % of width
+              child: _buildImage(),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    _buildTitlePrice(),
+                    const Spacer(),
+                    _buildRatingReview(),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  Widget _buildAlternateCardContent() {
+    return Row(
+      children: [
+        _buildImage(),
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(16),
             child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                SizedBox(
-                  width: width,
-                  height: width / 1.25,
-                  child: ClipRRect(
-                    borderRadius: const BorderRadius.vertical(top: Radius.circular(10.0)),
-                    child: Image.network(
-                      product.imageUrl,
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: Padding(
-                    padding: const EdgeInsets.fromLTRB(8.0, 8.0, 8.0, 8.0),
-                    child: Text(
-                      product.name,
-                      style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                            fontWeight: FontWeight.bold,
-                          ),
-                      maxLines: 3,
-                      overflow: TextOverflow.ellipsis,
-                    ),
-                  ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(8.0, 0, 8.0, 8.0),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Text(
-                        'USD ${product.price.toStringAsFixed(2)}',
-                        style: Theme.of(context).textTheme.titleSmall!.copyWith(
-                              fontWeight: FontWeight.bold,
-                            ),
-                      ),
-                      const SizedBox(height: 4.0),
-                      Row(
-                        children: [
-                          const Icon(Icons.star, size: 16.0, color: Colors.amber),
-                          const SizedBox(width: 4.0),
-                          Text(
-                            product.rating,
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-                          const SizedBox(width: 4.0),
-                          Text(
-                            '${product.reviewCount} Reviews',
-                            style: Theme.of(context).textTheme.bodySmall,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
+                _buildTitlePrice(maxLines: 2),
+                const Spacer(),
+                _buildRatingReview(),
               ],
             ),
           ),
         ),
-      ),
+      ],
+    );
+  }
+
+  Widget _buildImage() {
+    return Image.network(
+      product.imageUrl,
+      fit: BoxFit.cover,
+    );
+  }
+
+  Widget _buildTitlePrice({int? maxLines}) {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          product.name,
+          style: Theme.of(Get.context!).textTheme.titleSmall!.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+          maxLines: maxLines ?? 3,
+          overflow: TextOverflow.ellipsis,
+        ),
+        Container(
+          height: 5,
+        ),
+        Text(
+          'USD ${product.price.toStringAsFixed(2)}',
+          style: Theme.of(Get.context!).textTheme.titleSmall!.copyWith(
+                fontWeight: FontWeight.bold,
+              ),
+        ),
+      ],
+    );
+  }
+
+  Widget _buildRatingReview() {
+    return Row(
+      children: [
+        const Icon(Icons.star, size: 16.0, color: Colors.amber),
+        const SizedBox(width: 4.0),
+        Text(
+          product.rating,
+          style: Theme.of(Get.context!).textTheme.bodySmall,
+        ),
+        const SizedBox(width: 4.0),
+        Text(
+          '${product.reviewCount} Reviews',
+          style: Theme.of(Get.context!).textTheme.bodySmall,
+        ),
+      ],
     );
   }
 }

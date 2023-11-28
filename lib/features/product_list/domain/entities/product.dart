43de52099dcaf673b84cd210be6cf2e52e52e300
nicholas-stancio-saka <work.nicholasstancio@gmail.com>
2023-11-28 20:48:32 +0700

    remove unused functions

--- lib/features/product_list/domain/entities/product.dart
+++ lib/features/product_list/domain/entities/product.dart
@@ -1,5 +1,3 @@
-import 'dart:convert';
-
 class Product {
   final int id;
   final String imageUrl;
@@ -18,28 +16,4 @@ class Product {
     required this.reviewCount,
     required this.price,
   });
-
-  factory Product.fromRawJson(String str) => Product.fromJson(json.decode(str));
-
-  String toRawJson() => json.encode(toJson());
-
-  factory Product.fromJson(Map<String, dynamic> json) => Product(
-        id: json["id"],
-        imageUrl: json["image_url"],
-        name: json["name"],
-        description: json["description"],
-        rating: json["rating"],
-        reviewCount: json["review_count"],
-        price: json["price"]?.toDouble(),
-      );
-
-  Map<String, dynamic> toJson() => {
-        "id": id,
-        "image_url": imageUrl,
-        "name": name,
-        "description": description,
-        "rating": rating,
-        "review_count": reviewCount,
-        "price": price,
-      };
 }

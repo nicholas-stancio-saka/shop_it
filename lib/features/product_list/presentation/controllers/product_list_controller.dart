f2a66ad1122845ed911fed5d533410ef8ef21561
nicholas-stancio-saka <work.nicholasstancio@gmail.com>
2023-11-29 12:33:37 +0700

    Better state handling

--- lib/features/product_list/presentation/controllers/product_list_controller.dart
+++ lib/features/product_list/presentation/controllers/product_list_controller.dart
@@ -1,4 +1,6 @@
 import 'package:get/get.dart';
+import 'package:shop_it/core/error/error_model.dart';
+import 'package:shop_it/core/error/error_utility.dart';
 import 'package:shop_it/features/product_list/domain/entities/category.dart';
 import 'package:shop_it/features/product_list/domain/entities/product.dart';
 import 'package:shop_it/features/product_list/domain/usecases/category.dart';
@@ -13,7 +15,8 @@ class ProductListController extends GetxController {
   ProductListController(this._productUseCase, this._categoryUseCase);
 
   // State Info and obs
-  final viewState = ViewStateInfo(state: ViewState.loading).obs;
+  final viewStateCategory = ViewStateInfo(state: ViewState.loading).obs;
+  final viewStateProducts = ViewStateInfo(state: ViewState.loading).obs;
 
   // Data
   final categoryList = <Category>[].obs;
@@ -22,37 +25,64 @@ class ProductListController extends GetxController {
   late Category selectedCategory;
 
   Future<void> onRefresh() async {
+    await _fetchCategories();
+    await updateCategory(selectedCategory);
+  }
+
+  Future<void> updateCategory(Category newCategory) async {
     try {
-      viewState.value = ViewStateInfo(state: ViewState.loading);
+      viewStateProducts.value = ViewStateInfo(state: ViewState.loading);
 
-      await _fetchCategories();
-      await _fetchProducts(selectedCategory.id);
+      selectedCategory = newCategory;
 
-      viewState.value = ViewStateInfo(state: ViewState.success);
-    } catch (e) {
-      viewState.value = ViewStateInfo(
+      if (newCategory.id == 'all') {
+        await _fetchAllProducts();
+      } else {
+        await _fetchProducts(newCategory.id);
+      }
+
+      viewStateProducts.value = ViewStateInfo(state: ViewState.success);
+    } catch (e, st) {
+      viewStateProducts.value = ViewStateInfo(
         state: ViewState.failed,
         message: 'Unable to fetch data, please try again later!',
         callback: onRefresh,
       );
-    }
-  }
 
-  Future<void> updateCategory(Category newCategory) async {
-    selectedCategory = newCategory;
-
-    if (newCategory.id == 'all') {
-      _fetchAllProducts();
-    } else {
-      await _fetchProducts(newCategory.id);
+      // Log Error
+      AppErrorUtility.logError(ErrorModel(
+        type: ErrorType.presentation,
+        error: e,
+        stackTrace: st,
+      ));
     }
   }
 
   Future<void> _fetchCategories() async {
-    categoryList.value = await _categoryUseCase.call();
+    try {
+      viewStateCategory.value = ViewStateInfo(state: ViewState.loading);
 
-    // Insert the All category
-    categoryList.insert(0, Category(id: 'all', name: 'all'));
+      categoryList.value = await _categoryUseCase.call();
+
+      // Insert the All category
+      categoryList.insert(0, Category(id: 'all', name: 'all'));
+      selectedCategory = categoryList[0];
+
+      viewStateCategory.value = ViewStateInfo(state: ViewState.success);
+    } catch (e, st) {
+      viewStateCategory.value = ViewStateInfo(
+        state: ViewState.failed,
+        message: 'Unable to fetch data, please try again later!',
+        callback: onRefresh,
+      );
+
+      // Log Error
+      AppErrorUtility.logError(ErrorModel(
+        type: ErrorType.presentation,
+        error: e,
+        stackTrace: st,
+      ));
+    }
   }
 
   Future<void> _fetchProducts(String categoryId) async {
--- lib/features/product_list/presentation/views/product_list.dart
+++ lib/features/product_list/presentation/views/product_list.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:shimmer/shimmer.dart';
 import 'package:shop_it/features/product_list/domain/entities/category.dart';
 import 'package:shop_it/features/product_list/presentation/controllers/product_list_controller.dart';
 import 'package:shop_it/features/product_list/presentation/widgets/category_row.dart';
@@ -14,64 +15,98 @@ class ProductListScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Obx(() {
-      if (productListController.viewState.value.state == ViewState.loading) {
-        return const Center(child: CircularProgressIndicator()); // TODO: Create better loading with Shimmer
-      } else if (productListController.viewState.value.state == ViewState.failed) {
-        return AppErrorPage(
-          message: productListController.viewState.value.message,
-          callback: productListController.viewState.value.callback,
-        );
-      }
+    return Padding(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: double.infinity,
+          ),
+          _buildTitleBar('John'), // TODO: Use the local storage name, defaults to 'Guest'
+          Container(
+            height: 20,
+          ),
+          Obx(() {
+            if (productListController.viewStateCategory.value.state == ViewState.loading) {
+              return Shimmer.fromColors(
+                  baseColor: Colors.grey[300]!,
+                  highlightColor: Colors.grey[100]!,
+                  child: Container(
+                    height: 40,
+                    width: double.infinity,
+                    color: Colors.grey[300]!,
+                  ));
 
-      return Padding(
-        padding: const EdgeInsets.all(16),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.start,
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Container(
-              width: double.infinity,
-            ),
-            _buildTitleBar('John'), // TODO: Use the local storage name, defaults to 'Guest'
-            Container(
-              height: 20,
-            ),
-            CategoryRow(
+              // TODO: Do better shimmer
+            } else if (productListController.viewStateCategory.value.state == ViewState.failed) {
+              return Container();
+            }
+
+            return CategoryRow(
               categories: productListController.categoryList,
               onCategoryTap: (Category category) {
                 productListController.updateCategory(category);
               },
-            ),
-            Container(
-              height: 20,
-            ),
-            Expanded(
-              child: Obx(
-                () => GridView.builder(
-                  padding: const EdgeInsets.all(4.0),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                    crossAxisSpacing: 4.0,
-                    mainAxisSpacing: 4.0,
-                    childAspectRatio: 0.6,
-                  ),
-                  itemCount: productListController.productList.length,
-                  itemBuilder: (BuildContext context, int index) {
-                    return ProductCard(
-                      product: productListController.productList[index],
-                      onTap: () {
-                        Get.toNamed('/product_detail', arguments: productListController.productList[index]);
+            );
+          }),
+          Container(
+            height: 20,
+          ),
+          Expanded(
+            child: Obx(() {
+              if (productListController.viewStateProducts.value.state == ViewState.loading) {
+                return Shimmer.fromColors(
+                    baseColor: Colors.grey[300]!,
+                    highlightColor: Colors.grey[100]!,
+                    child: GridView.builder(
+                      padding: const EdgeInsets.all(4.0),
+                      gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        crossAxisSpacing: 4.0,
+                        mainAxisSpacing: 4.0,
+                        childAspectRatio: 0.6,
+                      ),
+                      itemCount: 6,
+                      itemBuilder: (BuildContext context, int index) {
+                        return Container(
+                          color: Colors.grey[300]!,
+                        );
                       },
-                    );
-                  },
+                    ));
+
+                // TODO: Do better shimmer
+              } else if (productListController.viewStateProducts.value.state == ViewState.failed) {
+                return AppErrorPage(
+                  message: productListController.viewStateProducts.value.message,
+                  callback: productListController.viewStateProducts.value.callback,
+                );
+              }
+
+              return GridView.builder(
+                padding: const EdgeInsets.all(4.0),
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 2,
+                  crossAxisSpacing: 4.0,
+                  mainAxisSpacing: 4.0,
+                  childAspectRatio: 0.6,
                 ),
-              ),
-            ),
-          ],
-        ),
-      );
-    });
+                itemCount: productListController.productList.length,
+                itemBuilder: (BuildContext context, int index) {
+                  return ProductCard(
+                    product: productListController.productList[index],
+                    onTap: () {
+                      Get.toNamed('/product_detail', arguments: productListController.productList[index]);
+                    },
+                  );
+                },
+              );
+            }),
+          ),
+        ],
+      ),
+    );
   }
 
   Widget _buildTitleBar(String userName) {
